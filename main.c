645803e3acb3463cc6fed2c5d8ab1043de5e6b7d
Riley de Man <rileydeman@gmail.com>
2025-01-31 11:05:21 +0100

    Start at setting up the game
    Start at setting up the game

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Variables
-char line[100] = "===============================================";
+char mainLine[100] = "======================================================";
+char secondaryLine[100] = "------------------------------------------------------";
 
 // Clean terminal function
 void cleanTerminal() {
@@ -25,15 +27,42 @@ void cleanTerminal() {
 
 }
 
+// Setup Game function
+void setupGame() {
+    printf("\nDo you want to host or join the server? ['h' = host, 'j' = join]\n> ");
+    char mode;
+    scanf("%c", &mode);
+
+    if (mode == 'h') {
+        printf("Host Game\n");
+    } else if (mode == 'j') {
+        printf("Join Game\n");
+    } else {
+        while (getchar() != '\n' && getchar() != EOF);
+        printf("\n%s\n", secondaryLine);
+        setupGame();
+    }
+}
+
 // Main function
 int main() {
+    // Start up info
     printf("TIC TAC TOE\n");
+    printf("A multiplayer game over the internet\n");
     printf("\nMade by rileydeman\n");
+    printf("\n%s\n", mainLine);
 
+    // Program
+    setupGame();
+
+
+    // Program restarter
+    while (getchar() != '\n' && getchar() != EOF);
     printf("\n> Do you want to restart the program? (y/n) \n> ");
     char answer;
     scanf(" %c", &answer);
     if (answer == 'y') {
+        while (getchar() != '\n' && getchar() != EOF);
         cleanTerminal();
         main();
     }
